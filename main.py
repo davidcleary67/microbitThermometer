0020d463126d29da04800e4256d55d259b7fb4a5
davidcleary67 <87801023+davidcleary67@users.noreply.github.com>
2022-10-23 11:42:40 +1100

    Update pxt.json, main.blocks, main.py

--- main.py
+++ main.py
@@ -1,26 +1,122 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
+hands: List[Image] = []
+dir2 = 0
+idir = 0
+def showCompass():
+    global hands, dir2, idir
+    hands = [images.create_image("""
+            . . # . .
+                    . . # . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . # . . .
+                    . # . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    # # . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    # # # . .
+                    . . . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    . . # . .
+                    # # . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    . . # . .
+                    . # . . .
+                    . # . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    . . # . .
+                    . . # . .
+                    . . # . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . # .
+                    . . . # .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . # #
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . . .
+                    . . # # #
+                    . . . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . . .
+                    . . . # #
+                    . . # . .
+                    . . . . .
+                    . . . . .
+        """),
+        images.create_image("""
+            . . . # .
+                    . . . # .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+        """)]
+    while True:
+        dir2 = input.compass_heading() / 30
+        idir = Math.floor(dir2)
+        hands[idir].show_image(0)
+        if input.button_is_pressed(Button.B):
+            break
+    basic.show_number(input.compass_heading())
+
+def on_button_pressed_a():
+    basic.show_leds("""
         # . # . #
-        . # # # .
-        # # . # #
-        . # # # .
-        # . # . #
-        `)
-    basic.clearScreen()
+                . # # # .
+                # # . # #
+                . # # # .
+                # . # . #
+    """)
+    basic.clear_screen()
     basic.pause(1000)
-    basic.showNumber(input.temperature())
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
+    basic.show_number(input.temperature())
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    basic.show_leds("""
         . . # . .
-        . # # # .
-        # . # . #
-        . . # . .
-        . . # . .
-        `)
-    basic.clearScreen()
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+    """)
+    basic.clear_screen()
     basic.pause(1000)
-    basic.showNumber(input.compassHeading())
-})
-let im = []
-im[5].showImage(0)
+    showCompass()
+input.on_button_pressed(Button.B, on_button_pressed_b)
